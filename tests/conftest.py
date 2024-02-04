81301c2a77ccf6edd3979a0963c5b9ceb54d9276
necros <necros83@gmail.com>
2024-02-04 22:22:22 +0700

    fix after review

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,24 +11,20 @@ user_name = os.environ.get('LOGIN')
 password = os.environ.get('PASSWORD')
 
 
-@pytest.fixture(scope='function', autouse=False)
+@pytest.fixture(scope='function')
 def mobile_management_android():
     options = UiAutomator2Options().load_capabilities({
-        # Specify device and os_version for testing
         "platformName": "android",
         "platformVersion": "9.0",
         "deviceName": "Google Pixel 3",
 
-        # Set URL of the application under test
         "app": "bs://sample.app",
 
-        # Set other BrowserStack capabilities
         'bstack:options': {
             "projectName": "First Python project",
             "buildName": "browserstack-build-1",
             "sessionName": "BStack first_test",
 
-            # Set your access credentials
             "userName": user_name,
             "accessKey": password
         }
@@ -44,24 +40,20 @@ def mobile_management_android():
     browser.quit()
 
 
-@pytest.fixture(scope='function', autouse=False)
+@pytest.fixture(scope='function')
 def mobile_management_ios():
     options = XCUITestOptions().load_capabilities({
-        # Specify device and os_version for testing
         "deviceName": "iPhone 11 Pro",
         "platformName": "ios",
         "platformVersion": "13",
 
-        # Set URL of the application under test
         "app": "bs://sample.app",
 
-        # Set other BrowserStack capabilities
         'bstack:options': {
             "projectName": "First Python project1",
             "buildName": "browserstack-build-1-2",
             "sessionName": "BStack first_test-1",
 
-            # Set your access credentials
             "userName": user_name,
             "accessKey": password
         }
--- tests/tests_ios/test_ios.py
+++ tests/tests_ios/test_ios.py
@@ -5,8 +5,6 @@ from selene import browser, have, be
 
 def test_ios_search_wiki_Appium(mobile_management_ios):
     with step('I do not know'):
-        # Test case for the BrowserStack sample iOS app.
-        # If you have uploaded your app, update the test case here.
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Button")).click()
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Input")).type("hello@browserstack.com" + "\n")
         text_output = browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Output")).should(be.clickable)
